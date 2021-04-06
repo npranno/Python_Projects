b8a5edf79daf3f285fac70330fdf9991688cce82
npranno <77467876+npranno@users.noreply.github.com>
2021-04-06 11:50:24 -0700

    Create nice_or_mean.py
    Nice or mean game

---
+++ nice_or_mean.py
@@ -0,0 +1,78 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:/Users/npran/OneDrive/Desktop/Python Files from Course/nice_or_mean.py ========
+
+What is your name? 
+>>> nick
+
+Welcome, Nick!
+
+In this game, you will be greeted 
+by several different people. 
+You can choose to be nice or mean
+but at the end of the game your fate 
+will be sealed by your actions.
+
+Nick, your current total: 
+(0, Nice) and (0, Mean)
+
+A stranger approaches you for a 
+conversation. Will you be nice 
+or mean? (N/M) 
+>>>: m
+
+The stranger glares at you 
+menacingly and storms off...
+
+Nick, your current total: 
+(0, Nice) and (1, Mean)
+
+A stranger approaches you for a 
+conversation. Will you be nice 
+or mean? (N/M) 
+>>>: m
+
+The stranger glares at you 
+menacingly and storms off...
+
+Nick, your current total: 
+(0, Nice) and (2, Mean)
+
+A stranger approaches you for a 
+conversation. Will you be nice 
+or mean? (N/M) 
+>>>: m
+
+The stranger glares at you 
+menacingly and storms off...
+
+Ahhh too bad, game over! 
+Nick, you live in a dirty beat-up 
+van by the river, wretched and alone!
+
+Do you want to play again? (y/n):
+>>> n
+
+Oh, so sad, sorry to see you go!
+>>> 
+========= RESTART: C:/Users/npran/OneDrive/Desktop/Python Files from Course/nice_or_mean.py ========
+
+What is your name? 
+>>> nick
+
+Welcome, NICK!
+
+In this game, you will be greeted 
+by several different people. 
+You can choose to be nice or mean
+but at the end of the game your fate 
+will be sealed by your actions.
+
+NICK, your current total: 
+(0, Nice) and (0, Mean)
+
+A stranger approaches you for a 
+conversation. Will you be nice 
+or mean? (N/M) 
+>>>: 
